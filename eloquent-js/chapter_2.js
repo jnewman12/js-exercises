cb6e4f17a7ba6f19ad6170e4d9f781f14f50a846
James Newman <jamesmnewman1@gmail.com>
2014-11-04 23:27:42 -0800

    second part

--- eloquent-js/chapter_2.js
+++ eloquent-js/chapter_2.js
@@ -51,6 +51,96 @@ console.log(one + two);
 // Example: in a browser enviornment the variable alert holds a function that shows a little dialog box with a message it is used:
 // alert ("Good Morning!");
 
+// a lot of functions are useful because of the side effects they produce
+
+console.log(Math.max(2, 4));
+console.log(Math.min(2, 4) + 100);
+
+
+// Browswers also contain other functions besides alert for popping up windows. You can ask the user an "ok/cancel" question using confirm. 
+// confirm("Shall we, then?");
+// This returns a boolean; true if the user clicks ok, false if the user clicks cancel
+
+// The prompt function can be used to ask an open question. A line of text can be typed into the dialog window, and the function will return this text as a string
+prompt('tell me everything you know.', '...');
+
+// When programs contain more than one statement, the statements are executed from top to bottom
+var theNumber = Number(prompt('Pick a number', ""));
+alert('Your number is the square root of ' + 
+	  theNumber * theNumber);
+
+// Conditional Execution (if/else)
+var theNumber = prompt('Pick a number', '');
+if (!isNaN(theNumber))
+	alert('Your number is the square root of ' +
+		theNumber * theNumber);
+
+// or
+var theNumber = Number(prompt('Pick a number', ''));
+if (!isNaN(theNumber))
+alert('Your number is the square root of ' + 
+	theNumber * theNumber);
+else
+	alert('Hey. Why did you not give me a number :(');
+
+// 'chaining'
+var num = Number(prompt('Pick a number', '0'));
+
+if (num < 10)
+  alert('Small');
+else if (num < 100)
+  alert('Medium');
+else
+  alert('Large');
+
+var number = 0;
+while (number <= 12) {
+	console.log(number);
+	number = number + 2;
+}    		
+// while loops executes the statement as long as the expression produces a value that is true when converted to Boolean type
+
+// Whenever you want to execute multiple statements inside a loop, wrap them in curly braces ({ and })
+// a sequence of statements wrapped in braces is calles a block.
+
+var result = 1;
+var counter = 0;
+while (counter < 10) {
+	result = result * 2;
+	counter = counter + 1;
+}
+console.log(result);
+
+// the do loop is a control structure similar to the while loop.
+// It is only different because a do loop always executes its body at least once, and it starts testing whether it should stop only after the first execution
+
+do {
+	var name = prompt('Who are you?');
+} while (!name);
+console.log(name);
+
+// for loop
+for (var number = 0; var number <= 12; number = number + 2)
+console.log(number);
+
+// this is 2^10 using for 
+var result = 1;
+for (var counter = 0; counter < 10; counter = counter + 1)
+	result = result * 2;
+console.log(result);
+
+// other than a loop returning false, break is another way of immediately jumping out of a loop
+// find a number greater than or equal to 20, divisible by 7
+for (var current = 20; ; current++) {
+	if (current % 7 == 0)
+		break;
+}  
+console.log(current);
+
+
+
+
+
 
 
 
