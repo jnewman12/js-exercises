df881c4b9bf80dda59978eb95e34e76e6f7a6340
James Newman <jamesmnewman1@gmail.com>
2014-11-06 13:44:07 -0800

    part 3

--- eloquent-js/chapter_4.js
+++ eloquent-js/chapter_4.js
@@ -80,3 +80,105 @@ console.log(tableFor('pizza', JOURNAL));
 // -> [76, 9, 4, 1]
 
 // Objects as Maps
+// instead of storing this data as an array, objects are a better way. 
+// using sqaure bracket access notation to create and read the properties and can use the in operator to test whether a given property exists
+
+var map = {};
+function storePhi(event, phi) {
+	map[event] = phi;
+}
+
+storePhi('pizza', 0.069);
+storePhi('touched tree', -0.081);
+console.log('pizza' in map);
+console.log(map['touched tree']);
+// 0.081
+
+// a map is a way to go from values in one domain to corresponding values in another domain
+
+// what if we want to final all events for which we have stored in a coefficient?
+// using a for loop isnt going to work because properties dont form a predictable series
+// this is a loop for going over properties of an object
+for (var event in map) {
+	console.log("The correlation for ' " + event + " 'is " map[event]);
+}
+// correlation for pizza is 0.069
+// correlation for touched tree is 0.081
+
+function gatherCorrelations(journal) {
+	var phis = {};
+	for (var entry = 0; entry < journal.length; entry ++) {
+		var events = journal[entry].events;
+		for (var i = 0; i < events.length; i++) {
+			var event = events[i];
+			if(!(event in phis))
+				phis[event] = phi(tableFor(event, journal));
+		}
+	}
+	return phis;
+}
+var correlations = gatherCorrelations(JOURNAL);
+console.log(correlations.pizza);
+
+
+// array methods for adding and removing things at the start of an array are called unshift, and shift
+var todoList = [];
+function rememberTo(task) {
+	todoList.push(task);
+}
+function whatIsNext() {
+	return todoList.shift();
+}
+function urgentlyRememberTo(task) {
+	todoList.unshift(task);
+}
+// the program manages tasks. You add tasks to the end of the list by calling rememberTo('eat')
+// when you're ready to do something, you call whatIsNext() to get and remove the front item from the list.
+// the urgentlyRememberTo adds a task but adds it to the front instead of to the back
+
+// indexOf has a sibling called lastIndexOf which starts searching for the given element at the end of the array instead of the front
+console.log([1, 2, 3, 4, 5].indexOf(2));
+console.log([1, 2, 3, 2, 1].lastIndexOf(2));
+
+function remove(array, index) {
+	return array.slice(0, index)
+	.concat(array.slice(index + 1));
+}
+console.log(remove(['a', 'b', 'c', 'd', 'e'], 2));
+
+// values of type string, number and boolean are not objects. They are immutable and cannot be changed
+
+// javascript does not warn you about defining a variable name that is already taken
+// Math Stuff
+function randomPointOnCircle(radius) {
+	var angle = Math.random()
+ * 2 * Math.PI;
+ return { x: radius * Math.cos(angle),
+          y: radius * Math.sin(angle)};
+}
+console.log(randomPointOnCircle(2));
+
+// the global scope can also be approached as an object in Javascript
+// in browsers, the global scope object is stored in the window variable
+var myVar = 10;
+console.log('myVar' in window);
+// true
+console.log(window.myVar);
+// 10
+
+// Exercises
+
+// 1. Sum of a range
+
+// 2. Reversing an Array
+
+// 3. A list
+
+// 4. Deep Comparison
+
+
+
+
+
+
+
