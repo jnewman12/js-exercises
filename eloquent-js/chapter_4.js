d6cc64c9ce6d2707bf3a769c089b4df248ecf868
James Newman <jamesmnewman1@gmail.com>
2014-11-06 14:56:02 -0800

    exercises done

--- eloquent-js/chapter_4.js
+++ eloquent-js/chapter_4.js
@@ -193,10 +193,73 @@ function range(start, end, step) {
 }
 
 // 2. Reversing an Array
+/*The first, reverseArray, takes an array as argument and produces a new array that has the same elements in the inverse order.*/
+function reverseArray(arr) {
+    var reversed = arr.slice().reverse();
+    return reversed;
+}
+reverseArray([1,2,3]);
+
+function reverseArrayInPlace(array) {
+  for (var i = 0; i < Math.floor(array.length / 2); i++) {
+    var old = array[i];
+    array[i] = array[array.length - 1 - i];
+    array[array.length - 1 - i] = old;
+  }
+  return array;
+}
 
 // 3. A list
+function arrayToList(array) {
+	var list = null;
+	for (var i = array.length - 1; i >= 0; i--)
+		list = {value: array[i], rest: list};
+	return list;
+}
 
+function listToArray() {
+	var array = [];
+	for (var node = list; node; node = node.rest)
+		array.push(node.value);
+	return array;
+}
+
+function prepend(value, list) {
+	return { value: value, rest: list};
+}
+
+function nth(list, n) {
+	if (!list)
+		return undefined;
+	else if (n == 0)
+		return list.value;
+	else
+		return nth(list.rest, n - 1);
+}
+console.log(arrayToList([10,20]));
 // 4. Deep Comparison
+// not my code, coldnt figure it out
+function deepEqual(a, b) {
+  if (a === b) return true;
+  
+  if (a == null || typeof a != "object" ||
+      b == null || typeof b != "object")
+    return false;
+  
+  var propsInA = 0, propsInB = 0;
+
+  for (var prop in a)
+    propsInA += 1;
+
+  for (var prop in b) {
+    propsInB += 1;
+    if (!(prop in a) || !deepEqual(a[prop], b[prop]))
+      return false;
+  }
+
+  return propsInA == propsInB;
+}
+
 
 
 
