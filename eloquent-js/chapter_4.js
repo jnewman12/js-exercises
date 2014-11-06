a5e601d29aa075e4f8303aaa764c04a70e704ba6
James Newman <jamesmnewman1@gmail.com>
2014-11-05 23:51:24 -0800

    part 1

---
+++ eloquent-js/chapter_4.js
@@ -0,0 +1,42 @@
+// Chapter 4
+// Data Structures: Objects & Arrays
+
+// objects allows to group values (including other objects) together
+
+// in js, accessing properties (arrays or hashes) can be done by 
+// value.x and value['x'] but they are different
+// when using a dot, the part after the dot must be a valid variable name and it directly becomes the property
+// when using square brackets, the expression between the brackets is evalutated to get the property name.
+// value.x fetches the property of value named x
+// value['x'] trues to evaluate the expression x and uses the result as property name
+
+// some fun with arrays
+var mack = [];
+mack.push("Mack");
+mack.push("the", "Knife");
+console.log(mack);
+console.log(mack.join(" "));
+console.log(mack.pop());
+console.log(mack);
+
+// one way to create an object is curly braces 
+var day1 = {
+	squirrel: false,
+	events: ['work', 'touched tree', 'pizza', 'running', 'television']
+};
+
+var anObject = {left: 1, right: 2};
+console.log(anObject.left);
+// → 1
+delete anObject.left;
+console.log(anObject.left);
+console.log("left" in anObject);
+console.log("right" in anObject);
+// the binary in operator, when applied to a string or object, returns a boolean value that indicates whether that object has that property. 
+// Arrays, are just a kind of object specialized for storing sequences of things
+
+
+
+
+
+
