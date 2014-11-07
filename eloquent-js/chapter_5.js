f57f44d4a7a02eadf11324ed1bb7eb10adc7cd1b
James Newman <jamesmnewman1@gmail.com>
2014-11-07 13:33:38 -0800

    exercise 3

--- eloquent-js/chapter_5.js
+++ eloquent-js/chapter_5.js
@@ -184,6 +184,36 @@ var differences = ancestry.filter(function(person) {
 console.log(average(differences));
 
 // 3. Historical Life Expectancy
+// average again to get the ages
+function average(array) {
+  function plus(a, b) { return a + b; }
+  return array.reduce(plus) / array.length;
+}
+
+// grouping 
+function groupBy(array, groupOf) {
+  var groups = {};
+  array.forEach(function(element) {
+    var groupName = groupOf(element);
+    if (groupName in groups)
+      groups[groupName].push(element);
+    else
+      groups[groupName] = [element];
+  });
+  return groups;
+}
+
+// getting century
+var byCentury = groupBy(ancestry, function(person) {
+  return Math.ceil(person.died / 100);
+});
+
+for (var century in byCentury) {
+  var ages = byCentury[century].map(function(person) {
+    return person.died - person.born;
+  });
+  console.log(century + ": " + average(ages));
+}
 
 // 4. Every and Then Some
 
