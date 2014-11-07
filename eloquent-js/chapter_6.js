860162ec0c5221b1651d0cac0513fb6c21b2121f
James Newman <jamesmnewman1@gmail.com>
2014-11-07 13:50:43 -0800

    chapter 6

---
+++ eloquent-js/chapter_6.js
@@ -0,0 +1,4 @@
+// Chapter 6
+// Secret Life of Objects
+
+// an object is a hard shell that hides complexity inside it and offers us a few knobs and connectors (methods) that present and interface through which the object is to be used
