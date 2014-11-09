b3b8a37666eb242de3a4c101256aa81230bba976
James Newman <jamesmnewman1@gmail.com>
2014-11-08 17:44:35 -0800

    objects

--- js-is-sexy/objects.js
+++ js-is-sexy/objects.js
@@ -53,13 +53,122 @@ console.log(person.name); // Bryant
   // - enumerable; specified whether the property can be returned in a for/in loop
   // - writable; specified whether the property can be changed
 
-// Creating Objects  
+// Creating Objects
+
+// Objects Literals
+// empty object initialized using the object literal notation
+var myBooks = {};
+// object w/ 4 items, using object literal
+var mango = {
+	color: 'yellow', 
+	shape: 'round', 
+	sweetness: 8,
+
+	howSweetAmI: function() {
+		console.log('Good');
+	}
+}
+
+// Object Constructor
+// this is creating an object with Object Constructor. A constructor is a function used for initializing new objects, and you use the 'new' keyword to call the constructor
+var mango = new Object ();
+mango.color = 'yellow';
+mango.shape = 'round';
+mango.sweetness = 8;
+
+mango.howSweetAmI = function () {
+	console.log('Good');
+}
+// objects can contain any other data type, including Numbers, Arrays, and even other Objects
 
 
+// If you had an application that displays fruits, all fruits would have properties such as color, shape, sweetness, cost, showname etc
+// it would be tedious to type this everytime you want to create a new fruit object
+var banana = {
+	color: 'yellow', 
+	sweetness: 8,
+	fruitName: 'Mango',
+	nativeToLand: ['South America', 'Central America'],
 
+	showName: function() {
+		console.log('This is' + this.fruitName);
+	},
+	nativeTo: function() {
+		this.nativeLand.forEach(function(eachCountry) {
+			console.log('Grown in:' + eachCountry);
+		});
+	}
+}
+// this above code would prove to be repetious doing this over and over
 
+// this is a better way
+// Constructor Pattern for Creating Objects
+function fruit(theColor, theSweetness, theFruitName, theNativeToLand) {
+	this.color = color;
+	this.sweetness = sweetness;
+	this.fruitName = theFruitName;
+	this.nativeToLand = theNativeToLand;
 
+	this.showName = function() {
+		console.log('This is a ' + this.fruitName);
+	}
 
+	this.nativeTo = function() {
+    this.nativeToLand.foreach(function(eachCountry) {
+    	console.log('Grown in:' + eachCountry);
+      });
+	}
+}
+// with this type of pattern in place, you can create fruits very easliy;
+var mangoFruit = new Fruit('Yellow', 8, 'Mango', ['South America', 'Central America', 'West Africa']);
+mangoFruit.showName();// This is a Mango
+mangoFruit.nativeTo();
+// Grown in: South America
+// Grown in: Central America
+// Grown in: West Africa
 
+var pineappleFruit = new Fruit('Brown', 5, 'Pineapple', ['United States']);
+pineappleFruit.showName();// this is a pineapple
 
+// this above method really shows how javascript can use inheritance
+// an inherited property is defined on the objects prototype property. 
+// For example: someObject.prototype.firstName = 'rich'
+
+// an own property is defined directly on the object itself
+
+// create an object
+var aMango = new Fruit();
+// now we define the mangoSpice property directly on the aMango object
+// because we define the mangoSpice property directly on the aMango object, it is an own property because of aMango, not an inherited property
+aMango.mangoSpice = 'some value';
+
+// to access a property of an object, we use object.property
+console.log(aMango.mangoSpice); // 'some value'
+// to invoke a method of an object, we use object.method(), for example
+aMango.printStuff = function() { return 'Printing';}
+// Now can we call the printStuff method
+aMango.printStuff();
+
+// Prototype pattern for creating objects
+function Fruit() {
+
+}
+Fruit.prototype.color = 'Yellow';
+Fruit.prototype.sweetness = 7;
+Fruit.prototype.fruitName = 'Generic fruit';
+Fruit.prototype.nativeToLand = 'USA';
+Fruit.prototype.showName = function() {
+	console.log('This is a ' + this.fruitName);
+}
+
+Fruit.prototype.nativeTo = function() {
+	console.log('Grown in:' + this.nativeToLand);
+}
+
+// And this is how we call the Fruit() constructor in this prototype pattern
+var mangoFruit = new Fruit();
+mangoFruit.showName();
+mangoFruit.nativeTo();
+// This is a generic Fruit
+// Grown in USA
 
